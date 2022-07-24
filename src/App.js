e06a63c8cda15c6b22a06bcabf56c85532be2335
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2022-07-24 01:34:32 -0300

    Desafio 22
    Alteração de pasta e estilização dos componentes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import './App.css';  
-import Soma from './Soma';  
-import Subtracao from './Subtracao';
-import Multiplicacao from './Multiplicacao';
-import Divisao from './Divisao';
+import Soma from './Desafio22/Soma';  
+import Subtracao from './Desafio22/Subtracao';
+import Multiplicacao from './Desafio22/Multiplicacao';
+import Divisao from './Desafio22/Divisao';
+import { Component } from 'react';
+
+const Calculadora = (props) => <h3>Calculadora:</h3>
 
 function App() {
   return (
     <div className="App">
+      <Calculadora/>
       <Soma a={5} b={3} />
       <Subtracao a={10} b={3} />
       <Divisao a={10} b={2} />
