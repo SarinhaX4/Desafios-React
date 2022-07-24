d15f42190609d027dc26402c680d92bb0d4a3e21
Sara Cristina <94648070+SarinhaX4@users.noreply.github.com>
2022-07-24 02:41:36 -0300

    Desafio 22

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import App from './Desafio22/App';
 
 test('renders learn react link', () => {
   render(<App />);
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './Desafio20/App';
+import App from './Desafio22/App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
